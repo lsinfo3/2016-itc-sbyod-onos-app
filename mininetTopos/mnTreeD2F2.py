cf01bef00fb45de31b1b62bdc60823d7c8ed5655
Lorenz Reinhart <lorenz.reinhart@stud-mail.uni-wuerzburg.de>
2016-06-01 15:06:35 +0200

    Mininet topo changed

--- mininetTopos/mnTreeD2F2.py
+++ mininetTopos/mnTreeD2F2.py
@@ -7,12 +7,22 @@ from mininet.net import Mininet
 from mininet.node import RemoteController
 from mininet.topolib import TreeTopo
 from mininet.cli import CLI
-from mininet.log import setLogLevel
+from mininet.log import setLogLevel, info
 
 if __name__=='__main__':
   setLogLevel('info')
+  #info('*** Creating tree topo with depth=2 and fanout=2\n')
   treeTopo = TreeTopo(depth=2,fanout=2)
+
+  #info('*** Creating network\n')
+  #net = Mininet(topo=treeTopo, autoSetMacs=True, build=False, listenPort=6633)
+
+  #info('*** Adding remote controller\n')
+  #net.addController(name='onos', controller=RemoteController, ip='172.0.0.1')
   net = Mininet(topo=treeTopo, controller=lambda name: RemoteController(name, defaulartarIP='127.0.0.1'), autoSetMacs=True, listenPort=6633)
+  #net.build()
   net.start()
+  #for h in net.hosts:
+   # h.cmd('dhclient')
   CLI(net)
   net.stop()
